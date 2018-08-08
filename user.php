ad285f36a8186a5dbf4cc5f521c1a4d9bc10c48d
Joe Shanahan <shans.joe@gmail.com>
2018-08-08 14:30:54 -0400

    initial commit

---
+++ user.php
@@ -0,0 +1,47 @@
+<?php
+    include_once 'header.php';
+?>
+<?php
+    include_once 'includes/dbh.inc.php';
+    $username=$_GET['username'];
+    $sql="SELECT * FROM users WHERE user_uid='" . $username ."'";
+    $result=mysqli_query($conn, $sql);
+    $row=mysqli_fetch_assoc($result);
+    $first=$row['user_first'];
+    $last=$row['user_last'];
+    $email=$row['user_email'];
+?>
+<html>
+    <form action="includes/upload.inc.php" method="post" enctype="multipart/form-data">
+        Select image to upload:
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <input type="submit" value="Upload Image" name="submit">
+    </form>
+    <h1>
+        <?php
+            echo $first . " " . $last;
+        ?>
+    </h1>
+    <p>Username:
+        <?php
+            echo $username;
+        ?>
+    </p>
+    <p>Email:
+        <?php
+            echo $email;
+        ?>
+    </p>
+    <p>
+        <?php
+            $path = 'accounts/' . $_SESSION['u_uid'] . '/pfp';
+            $files = scandir($path);
+            if (isset($files[2])){
+                
+            }
+        ?>
+    </p>
+</html>
+<?php
+    include_once 'footer.php';
+?> 
