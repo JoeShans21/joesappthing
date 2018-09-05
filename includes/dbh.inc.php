48dc1532f4459b19b9431b29540b8243e51c1bd2
Joe Shanahan <shans.joe@gmail.com>
2018-09-05 17:42:44 -0400

    ytr

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,10 +1,2 @@
 <?php
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-
-$conn = new mysqli($server, $username, $password, $db);
-?>
+$conn = mysqli_connect("localhost:3306", "root", "", "db");
